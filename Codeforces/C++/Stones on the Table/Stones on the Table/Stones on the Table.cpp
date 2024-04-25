64a77bd0048bfebc7382e22431cdb9d5ea05357a
DoDoz015 <adhamhussien67@gmail.com>
2024-04-25 19:51:52 +0200

    jj

---
+++ Codeforces/C++/Stones on the Table/Stones on the Table/Stones on the Table.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int main() {
+	int n;
+	int sto = 0;
+	cin >> n;
+	string s;
+	cin >> s;
+	for (int i = 0; i < n - 1; i++) {
+		if (s[i] == s[i + 1]) {
+			sto++;
+		}
+	}
+	cout << sto;
+}
